9d400510f0b8458ec37cea7c3a272406cd794d1f
Samuel Waugh <samjmwaugh@gmail.com>
2022-11-17 18:20:06 +0000

    included bottom-up

--- lec5-rod-cutting.py
+++ lec5-rod-cutting.py
@@ -1,3 +1,6 @@
+# Rod Cutting
+
+# Top Down
 prices = [1,5,8,9,10,17,17,20,24,30]
 
 def maxRevenue(p, n, r):
@@ -19,3 +22,21 @@ for i in range(1,11):
     r = [-1]*(i+1)
     print("{0}: {1}".format(i, maxRevenue(prices, i, r)))
 
+
+
+# Bottom Up
+prices = [1,5,8,9,10,17,17,20,24,30]
+
+def bottomUpRod(p, n):
+    r = [0]*(n+1)
+    for j in range(1, n+1):
+        q = -1
+        for i in range(1, j+1):
+            val = p[i-1] + r[j-i]
+            if val > q:
+                q = val
+        r[j] = q
+    return r[n]
+
+for i in range(1,11):
+    print("{0}: {1}".format(i, bottomUpRod(prices, i)))
