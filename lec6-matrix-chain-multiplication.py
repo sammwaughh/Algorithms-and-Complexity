c035a7cb5873cfcfee5d0ff0fe5bcb52ad2f29a3
Samuel Waugh <samjmwaugh@gmail.com>
2022-11-17 18:40:44 +0000

    top down done

--- lec6-matrix-chain-multiplication.py
+++ lec6-matrix-chain-multiplication.py
@@ -1,9 +1,32 @@
 # Optimal Matrix Chain Multiplications
 
-def minimumMultiplications(p):
-    return 0
-
-
+# Top Down
 p = [10,100,5,50]
-print(minimumMultiplications(p))
+n = len(p)-1
+
+m = []
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(None)
+    m.append(row)
+
+def mM(m, p, i, j):
+    if m[i][j] != None:
+        return m[i][j]
+    if i == j:
+        m[i][j] = 0
+    else:
+        q = None
+        for k in range(i, j):
+            if q != None:
+                val = mM(m, p, i, k) + mM(m,p,k+1,j) + p[i-1]*p[k]*p[j]
+                if val < q:
+                    q = val
+            else:
+                q = mM(m, p, i, k) + mM(m,p,k+1,j) + p[i-1]*p[k]*p[j]
+        m[i][j] = q
+    return m[i][j]
+
+print(mM(m, p, 0, n-1))
 
